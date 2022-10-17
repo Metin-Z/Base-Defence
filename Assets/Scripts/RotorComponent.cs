2821f8508aae17f6d08f58993eed1e08ffd2f301
Metin-Z <metinzehiroglu34@gmail.com>
2022-10-17 13:38:48 +0300

    helicopter

---
+++ Assets/Scripts/RotorComponent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RotorComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.DORotate(new Vector3(0, 360, 0), 0.5F, RotateMode.Fast);
+    }
+}
